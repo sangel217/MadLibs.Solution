b35aba5c3b0a53e9cb29738914d6dfe0a839bc09
Student <example@example.com>
2019-04-29 14:50:06 -0700

    add some layout improvements

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,22 +5,39 @@ namespace MadLibs.Controllers
 {
     public class HomeController : Controller
     {
-        // [Route("/story")]
-        // public ActionResult Story() {
-        // MadWords myMadWords = new MadWords();
-        // myMadWords.SetNoun("Dog");
-        // myMadWords.SetVerb("pooed");
-        // myMadWords.SetAdjective("smelly");
-        // return View(myMadWords);
+        
         [Route("/form")]
         public ActionResult Form() { return View(); }
         [Route("/story")]
-        public ActionResult Story(string noun, string verb, string adjective) 
+        public ActionResult Story(string location, string famousPerson, string adjective1, string sound, string animal1, string animal2, string animal3, string verb1, string typeOfPerson, string noun, string verb2, string store, string race, string goodBad, string famousWoman, string verb3, string group, string place, string game, string setting, string edVerb, string accomplishment, string pluralNoun, string athlete, string adjective2) 
         {
             MadWords myMadWords = new MadWords();
+            myMadWords.SetLocation(location);
+            myMadWords.SetFamousPerson(famousPerson);
+            myMadWords.SetAdjective1(adjective1);
+            myMadWords.SetSound(sound);
+            myMadWords.SetAnimal1(animal1);
+            myMadWords.SetAnimal2(animal2);
+            myMadWords.SetAnimal3(animal3);
+            myMadWords.SetVerb1(verb1);
+            myMadWords.SetTypeOfPerson(typeOfPerson);
             myMadWords.SetNoun(noun);
-            myMadWords.SetVerb(verb);
-            myMadWords.SetAdjective(adjective);
+            myMadWords.SetVerb2(verb2);
+            myMadWords.SetStore(store);
+            myMadWords.SetRace(race);
+            myMadWords.SetGoodBad(goodBad);
+            myMadWords.SetFamousWoman(famousWoman);
+            myMadWords.SetVerb3(verb3);
+            myMadWords.SetGroup(group);
+            myMadWords.SetPlace(place);
+            myMadWords.SetGame(game);
+            myMadWords.SetSetting(setting);
+            myMadWords.SetEdVerb(edVerb);
+            myMadWords.SetAccomplishment(accomplishment);
+            myMadWords.SetPluralNoun(pluralNoun);
+            myMadWords.SetAthlete(athlete);
+            myMadWords.SetAdjective2(adjective2);
+            
             return View(myMadWords);
         }
     }
--- MadLibs/Models/MadWords.cs
+++ MadLibs/Models/MadWords.cs
@@ -2,9 +2,120 @@ namespace MadLibs.Models
 {
     public class MadWords
     {
+        private string _location;
+        private string _famousPerson;
+        private string _adjective1;
+        private string _sound;
+        private string _animal1;
+        private string _animal2;
+        private string _animal3;
+        private string _verb1;
+        private string _typeOfPerson;
         private string _noun;
-        private string _verb;
-        private string _adjective;
+        private string _verb2;
+        private string _store;
+        private string _race;
+        private string _goodBad;
+        private string _famousWoman;
+        private string _verb3;
+        private string _group;
+        private string _place;
+        private string _game;
+        private string _setting;
+        private string _edVerb;
+        private string _accomplishment;
+        private string _pluralNoun;
+        private string _athlete;
+        private string _adjective2;
+        
+        public string GetLocation()
+        {
+            return _location;
+        }
+
+        public void SetLocation(string newLocation)
+        {
+            _location = newLocation;
+        }
+
+        public string GetFamousPerson()
+        {
+            return _famousPerson;
+        }
+        public void SetFamousPerson(string newFamousPerson)
+        {
+            _famousPerson = newFamousPerson;
+        }
+
+        public string GetAdjective1()
+        {
+            return _adjective1;
+
+        }
+        public void SetAdjective1(string newAdjective1)
+        {
+            _adjective1 = newAdjective1;
+        }
+
+        public string GetSound()
+        {
+            return _sound;
+        }
+
+        public void SetSound(string newSound)
+        {
+            _sound = newSound;
+        }
+
+        public string GetAnimal1()
+        {
+            return _animal1;
+        }
+
+        public void SetAnimal1(string newAnimal1)
+        {
+            _animal1 = newAnimal1;
+        }
+
+        public string GetAnimal2()
+        {
+            return _animal2;
+        }
+
+        public void SetAnimal2(string newAnimal2)
+        {
+            _animal2 = newAnimal2;
+        }
+
+        public string GetAnimal3()
+        {
+            return _animal3;
+        }
+
+        public void SetAnimal3(string newAnimal3)
+        {
+            _animal3 = newAnimal3;
+        }
+
+        public string GetVerb1()
+        {
+            return _verb1;
+        }
+
+        public void SetVerb1(string newVerb1)
+        {
+            _verb1 = newVerb1;
+        }
+
+        public string GetTypeOfPerson()
+        {
+            return _typeOfPerson;
+        }
+
+        public void SetTypeOfPerson(string newTypeOfPerson)
+        {
+            _typeOfPerson = newTypeOfPerson;
+        }
 
         public string GetNoun()
         {
@@ -16,23 +127,154 @@ namespace MadLibs.Models
             _noun = newNoun;
         }
 
-        public string GetVerb()
+        public string GetVerb2()
         {
-            return _verb;
-        }
-        public void SetVerb(string newVerb)
-        {
-            _verb = newVerb;
+            return _verb2;
         }
 
-        public string GetAdjective()
+        public void SetVerb2(string newVerb2)
         {
-            return _adjective;
-
+            _verb2 = newVerb2;
         }
-        public void SetAdjective(string newAdjective)
+
+        public string GetStore()
         {
-            _adjective = newAdjective;
+            return _store;
+        }
+
+        public void SetStore(string newStore)
+        {
+            _store = newStore;
+        }
+
+        public string GetRace()
+        {
+            return _race;
+        }
+
+        public void SetRace(string newRace)
+        {
+            _race = newRace;
+        }
+
+        public string GetGoodBad()
+        {
+            return _goodBad;
+        }
+
+        public void SetGoodBad(string newGoodBad)
+        {
+            _goodBad = newGoodBad;
+        }
+
+        public string GetFamousWoman()
+        {
+            return _famousWoman;
+        }
+
+        public void SetFamousWoman(string newFamousWoman)
+        {
+            _famousWoman = newFamousWoman;
+        }
+
+        public string GetVerb3()
+        {
+            return _verb3;
+        }
+
+        public void SetVerb3(string newVerb3)
+        {
+            _verb3 = newVerb3;
+        }
+
+        public string GetGroup()
+        {
+            return _group;
+        }
+
+        public void SetGroup(string newGroup)
+        {
+            _group = newGroup;
+        }
+
+        public string GetPlace()
+        {
+            return _place;
+        }
+
+        public void SetPlace(string newPlace)
+        {
+            _place = newPlace;
+        }
+
+        public string GetGame()
+        {
+            return _game;
+        }
+
+        public void SetGame(string newGame)
+        {
+            _game = newGame;
+        }
+
+        public string GetSetting()
+        {
+            return _setting;
+        }
+
+        public void SetSetting(string newSetting)
+        {
+            _setting = newSetting;
+        }
+
+        public string GetEdVerb()
+        {
+            return _edVerb;
+        }
+
+        public void SetEdVerb(string newEdVerb)
+        {
+            _edVerb = newEdVerb;
+        }
+
+        public string GetAccomplishment()
+        {
+            return _accomplishment;
+        }
+
+        public void SetAccomplishment(string newAccomplishment)
+        {
+            _accomplishment = newAccomplishment;
+        }
+
+        public string GetPluralNoun()
+        {
+            return _pluralNoun;
+        }
+
+        public void SetPluralNoun(string newPluralNoun)
+        {
+            _pluralNoun = newPluralNoun;
+        }
+
+        public string GetAthlete()
+        {
+            return _athlete;
+        }
+
+        public void SetAthlete(string newAthlete)
+        {
+            _athlete = newAthlete;
+        }
+
+        public string GetAdjective2()
+        {
+            return _adjective2;
+        }
+
+        public void SetAdjective2(string newAdjective2)
+        {
+            _adjective2 = newAdjective2;
         }
 
     }  
